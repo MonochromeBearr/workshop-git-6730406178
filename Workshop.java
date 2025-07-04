a71765c65315c57dfbdd2b9a2d374b2dd0fa97e2
kitsada babthong <kritsada.ba@kkumail.com>
2025-07-04 15:20:30 +0700

    update age

--- Workshop.java
+++ Workshop.java
@@ -1,6 +1,8 @@
 public class Workshop {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("JIDAPA");
+        int age = 5;
+        System.out.println(5);
+        System.out.println("nice panda");
     }
 }
